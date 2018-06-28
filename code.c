b83547c5611880e64a334da3715507809722e880
lovely127 <37863264+lovely127@users.noreply.github.com>
2018-06-28 10:54:01 +0530

    Update code.c

--- code.c
+++ code.c
@@ -12,53 +12,29 @@
 #include <stdlib.h>
 
 #define MSR_TSC	0x10
-#define MSR_NEHALEM_PLATFORM_INFO	0xCE
-#define MSR_NEHALEM_TURBO_RATIO_LIMIT	0x1AD
 #define MSR_APERF	0xE8
 #define MSR_MPERF	0xE7
-#define MSR_PKG_C3_RESIDENCY	0x3F8
 #define MSR_PKG_C6_RESIDENCY	0x3F9
-#define MSR_PKG_C7_RESIDENCY	0x3FA
-#define MSR_CORE_C3_RESIDENCY	0x3FC
-#define MSR_CORE_C3_RESIDENCY	0x3FC
-#define MSR_CORE_C3_RESIDENCY	0x3FC
-#define MSR_CORE_C6_RESIDENCY	0x3FD
-
 
 unsigned int interval_sec = 5;	/* set with -i interval_sec */
-unsigned int verbose;		/* set with -v */
 unsigned int debug;		/* set with -d */
-unsigned int do_c0, skip_c0;
-unsigned int do_c1, skip_c1;
-unsigned int do_c3;
-unsigned int do_c6;
 unsigned int do_pkg;
 unsigned int do_aperf = 1;	/* TBD set with CPUID */
 unsigned int iterations;
 unsigned int units = 1000000000.0;	/* Ghz etc */
 unsigned int do_non_stop_tsc;
 unsigned int do_nehalem_c_state_residency;
-unsigned int do_nehalem_platform_info;
-unsigned int do_nehalem_turbo_ratio_limit;
-unsigned int do_nehalem_c_state_residency;
-
 int aperf_mperf_unstable;
-int backwards_count;
 char *progname;
-
 int num_cpus;
 int *fd_msr;
 
 typedef struct per_core_counters {
 	unsigned long long tsc;
-	unsigned long long c1;
-	unsigned long long c3;
-	unsigned long long c6;
 	unsigned long long aperf;
 	unsigned long long mperf;
-	unsigned long long pc3;
 	unsigned long long pc6;
-	unsigned long long pc7;
+	
 } PCC;
 
 PCC *pcc_even;
@@ -81,13 +57,7 @@ unsigned long long get_msr(int cpu, off_t offset)
 	}
 	return msr;
 }
-void print_header()
-{
-	
-	if (do_pkg) fprintf(stderr, "  %%pc6 ");
 
-	putc('\n', stderr);
-}
 
 void print_counters(PCC *cnt, PCC *avg) 
 {
@@ -99,7 +69,6 @@ void print_counters(PCC *cnt, PCC *avg)
 	if (debug)
 		fprintf(stderr, "%.6f sec\n", interval_float);
 
-	//print_header();
     
 		PCC *p;
 
@@ -110,9 +79,8 @@ void print_counters(PCC *cnt, PCC *avg)
 			fprintf(stderr, "avg");
 		} 
 		else 
-		{
+	    	{
 			p = &cnt[i];
-			//fprintf(stderr, "%4d",i);
 		}
 
 
@@ -141,18 +109,14 @@ void print_counters(PCC *cnt, PCC *avg)
 		
 		putc('\n', stderr);
 	}
-//}
+
 
 
 #define SUBTRACT_COUNTER(after, before, delta) (delta = (after - before), (before > after))
-
-
 int compute_delta(PCC *after, PCC *before)
 {
 	int i;
 	int error, error1, error2;
-
-	skip_c0 = skip_c1 = 0;
 
 	for (i = 0; i < num_cpus; ++i) 
 	{
@@ -168,17 +132,9 @@ int compute_delta(PCC *after, PCC *before)
 			fprintf(stderr, "or just the deep ones with \"processor.max_cstate=1\"\n");
 			_exit(-3);
 		}
-		error1 = SUBTRACT_COUNTER(after[i].c3, before[i].c3, pcc_delta[i].c3);
-		error2 = SUBTRACT_COUNTER(after[i].c6, before[i].c6, pcc_delta[i].c6);
-		if (error1 || error2)
-	        {
-			fprintf(stderr, "c3 or c6 residency counter went backwards\n");
-			_exit(-1);
-		}
-		error = SUBTRACT_COUNTER(after[i].pc3, before[i].pc3, pcc_delta[i].pc3);
+		
 		error1 = SUBTRACT_COUNTER(after[i].pc6, before[i].pc6, pcc_delta[i].pc6);
-		error2 = SUBTRACT_COUNTER(after[i].pc7, before[i].pc7, pcc_delta[i].pc7);
-		if (error || error1 || error2)
+		if ( error1 )
 	        {
 			fprintf(stderr, "package residency counter went backwards\n");
 			_exit(-1);
@@ -196,65 +152,15 @@ int compute_delta(PCC *after, PCC *before)
 
 				aperf_mperf_unstable = 1;
 			}
-			/*
-			 * mperf delta is likely a huge "positive" number
-			 * can not use it for calculating c0 time
-			 */
-			skip_c0 = 1;
-			skip_c1 = 1;
+			
 		}
-
-		/*
- 		 * As mperf and tsc collection are not atomic,
- 		 * it is possible for mperf's non-halted cycles
- 		 * to exceed TSC's all cycles: show c1l = 0% in that case.
- 		 */
-		if (pcc_delta[i].mperf > pcc_delta[i].tsc)
-			pcc_delta[i].c1 = 0;
-		else /* normal case, derive c1 */
-			pcc_delta[i].c1 = pcc_delta[i].tsc - pcc_delta[i].mperf - pcc_delta[i].c3 - pcc_delta[i].c6;
 
 		if (pcc_delta[i].mperf == 0)
 			pcc_delta[i].mperf = 1;	/* divide by 0 protection */
 	}
 }
 
-void compute_average(PCC *delta, PCC *avg)
-{
-	int i;
-	PCC *sum;
 
-	sum = calloc(1, sizeof(PCC));
-	if (sum == NULL)
-        {
-		perror("calloc sum");
-		_exit(-1);
-	}
-
-	for (i = 0; i < num_cpus; ++i)
-	{
-		sum->tsc += delta[i].tsc;
-		sum->c1 += delta[i].c1;
-		sum->c3 += delta[i].c3;
-		sum->c6 += delta[i].c6;
-		sum->aperf += delta[i].aperf;
-		sum->mperf += delta[i].mperf;
-		sum->pc3 += delta[i].pc3;
-		sum->pc6 += delta[i].pc6;
-		sum->pc7 += delta[i].pc7;
-	}
-	avg->tsc = sum->tsc/num_cpus;
-	avg->c1 = sum->c1/num_cpus;
-	avg->c3 = sum->c3/num_cpus;
-	avg->c6 = sum->c6/num_cpus;
-	avg->aperf = sum->aperf/num_cpus;
-	avg->mperf = sum->mperf/num_cpus;
-	avg->pc3 = sum->pc3/num_cpus;
-	avg->pc6 = sum->pc6/num_cpus;
-	avg->pc7 = sum->pc7/num_cpus;
-
-	free(sum);
-}
 
 
 void get_counters(PCC *c)
@@ -264,13 +170,10 @@ void get_counters(PCC *c)
 	for (i = 0; i < num_cpus; ++i)
 	{
 		c[i].tsc = get_msr(i, MSR_TSC);
-		if (do_c3) c[i].c3 = get_msr(i, MSR_CORE_C3_RESIDENCY);
-		if (do_c6) c[i].c6 = get_msr(i, MSR_CORE_C6_RESIDENCY);
 		if (do_aperf) c[i].aperf = get_msr(i, MSR_APERF);
 		if (do_aperf) c[i].mperf = get_msr(i, MSR_MPERF);
-		if (do_pkg) c[i].pc3 = get_msr(i, MSR_PKG_C3_RESIDENCY);
 		if (do_pkg) c[i].pc6 = get_msr(i, MSR_PKG_C6_RESIDENCY);
-		if (do_pkg) c[i].pc7 = get_msr(i, MSR_PKG_C7_RESIDENCY);
+		
 	}
 
 }
@@ -288,7 +191,6 @@ void turbostat_loop()
 
 		compute_delta(pcc_odd, pcc_even);
 		timersub(&tv_odd, &tv_even, &tv_delta);
- 	        compute_average(pcc_delta, pcc_average);
 		print_counters(pcc_delta, pcc_average);
 
 		sleep(interval_sec);
@@ -296,7 +198,6 @@ void turbostat_loop()
 		gettimeofday(&tv_even, (struct timezone *)NULL);
 		compute_delta(pcc_even, pcc_odd);
 		timersub(&tv_even, &tv_odd, &tv_delta);
-		compute_average(pcc_delta, pcc_average);
 		print_counters(pcc_delta, pcc_average);
 	}
 }
@@ -311,60 +212,29 @@ void check_dev_msr() {
 	}
 }
 
-int has_nehalem_turbo_ratio_limit(unsigned int family, unsigned int model)
-{
-	if (family != 6)
-		return 0;
-
-	switch(model) {
-	case 0x1A:	/* Core i7, Xeon 5500 series - Nehalem */
-	case 0x1E:	/* Core i7 and i5 Processor - Lynnfield, Jasper Forest */
-	case 0x1F:	/* Core i7 and i5 Processor - Nehalem */
-	case 0x25:	/* Westmere */
-	case 0x2C:	/* Westmere */
-		return 1;
-	case 0x2E:	/* Nehalem-EX Xeon */
-	default:
-		return 0;
-	}
-}
-
 int has_non_stop_tsc(unsigned int family, unsigned int model)
 {
 	if (family != 6)
 		return 0;
 
-	switch(model) {
-	case 0x1A:	/* Core i7, Xeon 5500 series */
-	case 0x1E:	/* Core i7 and i5 Processor */
-	case 0x1F:	/* Core i7 and i5 Processor */
-	case 0x2E:	/* Nehalem Xeon */
+	switch(model)
+      {
 	case 0x25:	/* Westmere */
-	case 0x2C:	/* Westmere */
 		return 1;
 	default:
 		return 0;
 	}
 }
 
-
-
 void do_cpuid()
 {
 	unsigned int eax, ebx, ecx, edx, max_level;
 	char brand[16];
-	unsigned int fms, family, model, stepping, ht_capable;
+	unsigned int fms, family, model, stepping;
 
 	eax = ebx = ecx = edx = 0;
 
 	asm("cpuid" : "=a" (max_level), "=b" (ebx), "=c" (ecx), "=d" (edx) : "a" (0));
-
-	sprintf(brand, "%.4s%.4s%.4s", &ebx, &edx, &ecx);
-
-	if (strncmp(brand, "GenuineIntel", 12)) {
-		fprintf(stderr, "CPUID: %s GenuineIntel\n", brand);
-		_exit(-1);
-	}
 
 	asm("cpuid" : "=a" (fms), "=c" (ecx), "=d" (edx) : "a" (1) : "ebx");
 	family = (fms >> 8) & 0xf;
@@ -373,26 +243,11 @@ void do_cpuid()
 	if (family == 6 || family == 0xf)
 		model += ((fms >> 16) & 0xf) << 4;
 
-	if (!(edx & (1 << 5))) {
-		fprintf(stderr, "CPUID: no MSR\n");
-		_exit(-1);
-	}
-
-	ht_capable = edx & (1 << 28);
-
-	if (verbose)
-		fprintf(stderr, "CPUID %s %d levels family:model:stepping %d:%d:%d\n",
-			brand, max_level, family, model, stepping);
-
 	do_non_stop_tsc = has_non_stop_tsc(family, model);
-	do_nehalem_platform_info = do_non_stop_tsc;
 	do_nehalem_c_state_residency =  do_non_stop_tsc;
 
-	do_nehalem_turbo_ratio_limit = has_nehalem_turbo_ratio_limit(family, model);
+	
 }
-
-
-
 
 void turbostat_init()
 {
@@ -433,7 +288,7 @@ void turbostat_init()
 
 	do_cpuid();
 
-	do_c0 = do_c1 = do_c3 = do_c6 = do_pkg = do_nehalem_c_state_residency;
+	do_pkg = do_nehalem_c_state_residency;
 
 	check_dev_msr();
 
@@ -452,19 +307,12 @@ void turbostat_init()
 	num_cpus = i;
 	if (num_cpus == 0)
 		_exit(-1);
-
 	
 }
-
 
 int main(int argc, char **argv)
 {
-
 	turbostat_init();
-
-	turbostat_loop();
-	
-		
+	turbostat_loop();	
 	return 0;
 }
-
